f4d17d193e77166aa6e2f14553f23f585f8e770c
Marcos Batista <51379746+m4rc05-34t15t4@users.noreply.github.com>
2024-08-02 10:54:51 -0300

    Delete gerar_senha.php

--- gerar_senha.php
+++
@@ -1,7 +0,0 @@
-<?php
-    if (isset($_GET['senha'])) {
-        $senha = base64_encode($_GET['senha']);
-        echo "SENHA: " . $senha;
-    }
-    else header("Refresh: 1; URL=gerar_senha.php?senha=");
-?>
