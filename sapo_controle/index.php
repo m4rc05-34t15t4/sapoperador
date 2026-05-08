34dd8065fd2aab93da3b6ea7b6e7a9c7facb4e61
Marcos Batista <51379746+m4rc05-34t15t4@users.noreply.github.com>
2026-05-08 10:30:53 -0300

    Add files via upload

--- sapo_controle/index.php
+++ sapo_controle/index.php
@@ -49,81 +49,11 @@
     <!-- Bootstrap 5 CDN -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="./css/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="./css/styles.css?<?=$v?>">
 </head>
-<style>
-    body {
-        background-color: #f8f9fa;
-        padding-top: 100px;
-        min-height: 100vh;
-    }
-    #div-conteudo{
-        min-height: 100vh;
-    }
-    .centralizar {
-        max-width: 900px;
-        margin: 50px auto;
-    }
-    .table tr th {
-        background-color: #444;
-        color: white;
-        font-size: 18px;
-    }
-    table td, table th {
-        vertical-align: middle; /* Centraliza verticalmente */
-        text-align: center;    /* Centraliza horizontalmente (opcional) */
-    }
-    .input-fit {
-        width: auto !important;
-        min-width: fit-content;
-        display: inline-block;
-    }
-    .loading-overlay {
-        position: fixed;
-        top: 0; left: 0;
-        width: 100%; height: 100%;
-        background: rgba(0, 0, 0, 0.7); /* Escurece */
-        backdrop-filter: blur(8px);    /* Embaça */
-        z-index: 9999;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        color: white;
-    }
-    .subtitulo_coluna {
-        display: flex;
-        flex-direction: row;
-        align-items: center;      /* Alinha ícone e texto verticalmente */
-        justify-content: space-between; /* Texto na esquerda, ícone na direita */
-        width: 100%;              /* Garante que ocupe a largura da TH */
-        gap: 10px;
-    }
-    .texto_header {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        line-height: 1.1;
-        flex-grow: 1;/* Faz o texto ocupar o máximo de espaço */
-    }
-    .texto_header span:last-child{
-        font-size: 13px;
-        color: #999;
-    }
-    #tabela-usuarios thead th, #tabela-dados thead th {
-        position: sticky;
-        top: var(--header-height, 100px); /* Usa a variável, ou 0px se não existir */
-        z-index: 10; /* Garante que fique por cima das linhas */
-        box-shadow: inset 0 -1px 0 #dee2e6; /* Mantém a borda inferior visível */
-    }
-    header {
-        background: radial-gradient(circle, #f1f8e9 0%, #c8e6c9 60%, #a5d6a7 100%);
-        border-bottom: 2px solid #a5d6a7;
-    }
-</style>
 <body class="bg-light">
-
     <!-- Container Principal do Cabeçalho -->
-    <header class="fixed-top container-fluid shadow-sm p-2 mb-2">
+    <header class="fixed-top container-fluid p-2 mb-2">
         <div class="d-flex flex-column flex-md-row align-items-center justify-content-center">
             <!-- Lado Esquerdo: O GIF Estilizado -->
             <div class="text-center">
@@ -141,16 +71,16 @@
 
     <div id="div-conteudo" class="text-center w-fill p-3 pb-0 mb-0">
         <div class="text-center w-fill">
-            <form id="formFiltros" method="get" class="d-flex flex-wrap justify-content-center align-items-center g-2 mb-3">
+            <form id="formFiltros" method="get" class="d-flex flex-wrap justify-content-center align-items-center gap-3 mb-5">
                 <!-- Select Usuário -->
                 <div>
-                    <label class="form-label small text-center">Operador</label>
+                    <label class="form-label text-center">Operador</label>
                     <select id="userSelect" class="form-select w-auto" name="nome_guerra"></select>
                 </div>
 
                 <!-- Select Ano -->
                 <div>
-                    <label class="form-label small text-center">Ano</label>
+                    <label class="form-label text-center">Ano</label>
                     <select id="anoSelect" class="form-select w-auto" name="ano">
                         <option value="">Todos</option>
                     </select>
@@ -158,7 +88,7 @@
 
                 <!-- Select Mês -->
                 <div>
-                    <label class="form-label small text-center">Mês</label>
+                    <label class="form-label text-center">Mês</label>
                     <select id="mesSelect" class="form-select w-auto" name="mes">
                         <option value="">Todos</option>
                         <option value="1">Janeiro</option>
@@ -178,7 +108,7 @@
 
                 <!-- Select Semana -->
                 <div>
-                    <label class="form-label small text-center">Semana</label>
+                    <label class="form-label text-center">Semana</label>
                     <select id="semanaSelect" class="form-select w-auto" name="semana">
                         <option value="">Todas</option>
                     </select>
@@ -186,7 +116,7 @@
 
                 <!-- Select Lote -->
                 <div>
-                    <label class="form-label small text-center">Lote</label>
+                    <label class="form-label text-center">Lote</label>
                     <select id="loteSelect" class="form-select w-auto" name="lote">
                         <option value="">Todos</option>
                     </select>
@@ -194,7 +124,7 @@
 
                 <!-- Select Subfase -->
                 <div>
-                    <label class="form-label small text-center">Subfase</label>
+                    <label class="form-label text-center">Subfase</label>
                     <select id="subfaseSelect" class="form-select w-auto" name="subfase">
                         <option value="">Todas</option>
                     </select>
@@ -202,7 +132,7 @@
 
                 <!-- Select Bloco -->
                 <div>
-                    <label class="form-label small text-center">Bloco</label>
+                    <label class="form-label text-center">Bloco</label>
                     <select id="blocoSelect" class="form-select w-auto" name="bloco">
                         <option value="">Todos</option>
                     </select>
@@ -210,57 +140,67 @@
 
                 <!-- Datas (Período) -->
                 <div>
-                    <label class="form-label small">Início</label>
+                    <label class="form-label">Início</label>
                     <input type="date" name="data_inicio" class="form-control w-auto">
                 </div>
                 <div>
-                    <label class="form-label small">Fim</label>
+                    <label class="form-label">Fim</label>
                     <input type="date" name="data_fim" class="form-control w-auto">
                 </div>
                 
                 <!-- Redireciona para a página atual sem nenhum parâmetro GET -->
                 <div title="Limpar Filtros">
-                    <label class="form-label small"></label>
+                    <label class="form-label"></label>
                     <a href="http://10.46.137.15/sapo_controle/" class="btn btn-secondary w-100 mx-2 rounded">Limpar Filtros</i></a>
                 </div>
 
             </form>
 
             <div id='loader' class='spinner-border spinner-border-sm' role='status'></div>
+            
+            <hr/>
+            <h1 id="graficos">Gráfico</h1>
+            
             <div class='chart-container' style='height: 300px; min-width: 1000px;'>
                 <canvas id='graficoSemanal'></canvas>
             </div>
         </div>
 
-        <table id='tabela-usuarios' class='my-5 table table-striped table-bordered table-hover table-responsive'>
+        <hr/>
+        <h1 id="usuarios">Usuários</h1>
+
+        <table id='tabela-usuarios' class='my-2 mb-5 table table-striped table-bordered table-hover table-responsive border border-black'>
             <thead>
                 <tr style="cursor: pointer;">
-                    <th><span>Id</span> <i></i></th>
-                    <th><span>Operador</span> <i></i></th>
-                    <th><span>Com Atividade</span> <i></i></th>
-                    <th><span>Último Login</span> <i></i></th>
-                    <th><span>Execução</span> <i></i></th>
-                    <th><span>Correção</span> <i></i></th>
-                    <th><span>Revisão</span> <i></i></th>
-                    <th><span class="subtitulo_coluna"><div class="texto_header"><span>Ganchos Recebidos</span><span>Corrigidos / Total</span></div><i></i></span></th>
-                    <th><span class="subtitulo_coluna"><div class="texto_header"><span>Ganchos Aplicados</span><span>Corrigidos / Total</span></div><i></i></span></th>
-                    <th><span>Total Atividades</span> <i></i></th>
+                    <th id="th_usuarios_id"><span>Id</span> <i></i></th>
+                    <th id="th_usuarios_operador"><span>Operador</span> <i></i></th>
+                    <th id="th_usuarios_atv"><span>Com Atividade</span> <i></i></th>
+                    <th id="th_usuarios_login"><span>Último Login</span> <i></i></th>
+                    <th id="th_usuarios_execucao" mediana_cor="rgba(54, 162, 235, 0.5)"><span>Execução</span> <i></i></th>
+                    <th id="th_usuarios_correcao" mediana_cor="rgba(72, 240, 189, 0.5)"><span>Correção</span> <i></i></th>
+                    <th id="th_usuarios_revisao" mediana_cor="rgba(255, 99, 132, 0.5)"><span>Revisão</span> <i></i></th>
+                    <th id="th_usuarios_ganchos_rec"><span class="subtitulo_coluna"><div class="texto_header"><span>Ganchos Recebidos</span><span>Corrigidos / Total</span></div><i></i></span></th>
+                    <th id="th_usuarios_ganchos_apl"><span class="subtitulo_coluna"><div class="texto_header"><span>Ganchos Aplicados</span><span>Corrigidos / Total</span></div><i></i></span></th>
+                    <th id="th_usuarios_total_atv"><span>Total Atividades</span> <i></i></th>
                 </tr>
             </thead>
             <tbody id='corpoTabela-usuarios'></tbody>
         </table>
 
-        <table id='tabela-dados' class='my-5 table table-striped table-bordered table-hover table-responsive'>
+        <hr/>
+        <h1 id="unidades">Unidades de Tranalho</h1>
+
+        <table id='tabela-dados' class='my-2 mb-5 table table-striped table-bordered table-hover table-responsive border border-black'>
             <thead>
                 <tr style="cursor: pointer;">
-                    <th><span>Lote</span> <i></i></th>
-                    <th><span>Subfase</span> <i></i></th>
-                    <th><span>Bloco</span> <i></i></th>
-                    <th><span>Tipo</span> <i></i></th>
-                    <th><span>Operador</span> <i></i></th>
-                    <th><span>Total</span> <i></i></th>
-                    <th><span>Ganchos</span> <i></i></th>
-                    <th><span>Período Semana</span> <i></i></th>
+                    <th id="th_unidades_lote"><span>Lote</span> <i></i></th>
+                    <th id="th_unidades_subfase"><span>Subfase</span> <i></i></th>
+                    <th id="th_unidades_bloco"><span>Bloco</span> <i></i></th>
+                    <th id="th_unidades_tipo"><span>Tipo</span> <i></i></th>
+                    <th id="th_unidades_operador"><span>Operador</span> <i></i></th>
+                    <th id="th_unidades_total"><span>Total</span> <i></i></th>
+                    <th id="th_unidades_ganchos"><span>Ganchos</span> <i></i></th>
+                    <th id="th_unidades_periodo_semanal"><span>Período Semana</span> <i></i></th>
                 </tr>
             </thead>
             <tbody id='corpoTabela'></tbody>
@@ -291,6 +231,23 @@
             <div class="spinner-border text-light" style="width: 3rem; height: 3rem;" role="status"></div>
             <h4 class="m-3">Carregando dados...</h4>
         </div>
+         <!-- BARRA DE PROGRESSO -->
+        <div class="progress mt-2" style="width: 250px; height: 10px; background-color: rgba(255,255,255,0.2);">
+            <div id="barra-loader" class="progress-bar progress-bar-striped progress-bar-animated bg-success" 
+                 role="progressbar" style="width: 0%;"></div>
+        </div>
+    </div>
+
+    <div class="fab-container">
+        <button class="fab-btn" data-target="graficos" title="Gráficos">
+            <i class="bi bi-bar-chart-fill"></i>
+        </button>
+        <button class="fab-btn" data-target="usuarios" title="Usuários">
+            <i class="bi bi-people-fill"></i>
+        </button>
+        <button class="fab-btn" data-target="unidades" title="Unidades">
+            <i class="bi bi-briefcase-fill"></i>
+        </button>
     </div>
 
     <script src="js/chart.umd.min.js"></script>
